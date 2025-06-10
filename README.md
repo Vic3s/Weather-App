e0cd17d14ba8476ff0a8974d8467031c1baa5919
VaskoDaGama <vasko4da4gama@gmail.com>
2025-06-10 17:25:43 +0300

    Initial Commit.

---
+++ README.md
@@ -0,0 +1,1 @@
+A weather applictaion using React, Spring-Boot and the meteoblue API. 
---
+++ frontend/src/IndexPage.tsx
@@ -0,0 +1,23 @@
+import './index_page.css'
+
+const IndexPage = () => {
+
+  return (
+    <>
+    <div className='main-content'>
+      <div className='Heading'>
+        <h1>Weather Forcast</h1>
+      </div>
+
+      {/* <form onSubmit={}>
+        <div></div>
+        <input type="text" />
+      </form> */}
+    </div>
+    
+    
+    </>
+  )
+}
+
+export default IndexPage;
